e91ebb5bed5b7a49deb7ddc6cbf82b0a8c82f147
Shanto Noor <shantonoor900@gmail.com>
2024-05-25 21:00:22 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import cors from "cors";
+
+import { config } from "dotenv";
+import User from "./models/User.model.js";
+import mongoose from "mongoose";
+
+config({
+  path: ".env.local",
+});
+
+const app = express();
+
+// eslint-disable-next-line no-undef
+const port = process.env.port || 3000;
+
+// eslint-disable-next-line no-undef
+mongoose.connect(process.env.DB_URI);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  return res.send("Recipe Vault server is Running");
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    return res.send(await User.find(req.query));
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      return res.status(400).send(err.message);
+    } else {
+      return res.status(500).send("Something went wrong");
+    }
+  }
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const result = await user.save();
+    return res.status(201).send(result);
+  } catch (err) {
+    if (err.code === 11000) {
+      return res
+        .status(201)
+        .send(await User.findOne({ email: req.body.email }));
+    }
+    return res.status(err.code).send(err.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Recipe Vault server is listening on port ${port}`);
+});
---
+++ models/User.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  displayName: {
+    require: true,
+    type: String,
+  },
+  email: {
+    require: true,
+    type: String,
+    unique: true,
+  },
+  photoURL: {
+    require: false,
+    type: String,
+    default: "",
+  },
+  coin: {
+    require: false,
+    type: Number,
+    default: 50,
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
